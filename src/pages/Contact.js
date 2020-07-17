9955b364ae71580024acc92805a3ffffc545c794
Ray H <rjhern37@neiu.edu>
2020-07-17 16:22:04 -0500

    finishing up with general layout, need to work on styling

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,14 +5,17 @@ import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import SendIcon from '@material-ui/icons/Send';
+
 
 
 
 const useStyles = makeStyles((theme) => ({
     root: {
         '& .MuiTextField-root': {
-            margin: theme.spacing(1),
-            width: '25ch',
+            margin: theme.spacing(3),
+            width: '30ch',
         },
     },
 }));
@@ -22,44 +25,75 @@ export default function MultilineTextFields() {
 
 
     return (
-        <form className={classes.root} noValidate autoComplete="off">
+        <form className={classes.root} Validate autoComplete="off">
 
             <div>
-                 <Grid container spacing={3}>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>
-          
-                <Typography gutterBottom variant="h5" component="h2">
-                    Contact Me
-                </Typography>
-          </Paper>
-       
-                <TextField
-                    id="outlined-multiline-flexible"
-                    label="Name"
-                    type="text"
-                    multiline
-                    //   value={value}
-                    //   onChange={handleChange}
-                    variant="outlined"
-                />
-                <TextField
-                    id="outlined-textarea"
-                    label="Email"
-                    type="email"
-                    variant="outlined"
-                />
-                <TextField
-                    id="outlined-multiline-static"
-                    label="Message"
-                    type="text"
-                    multiline
-                    rows={4}
-                    //   defaultValue="Default Value"
-                    variant="outlined"
-                />
-             </Grid>
-             </Grid>
+                <Grid container spacing={3}>
+                    <Grid item xs={6}>
+                        <Paper className={classes.paper}>
+
+                            <Typography gutterBottom variant="h5" component="h2">
+                                Contact Me
+                            </Typography>
+                        </Paper>
+                    
+                    <Grid item md={6} justify="center">
+
+                        <Paper className={classes.paper}>
+
+                            <TextField
+                                id="outlined-multiline-flexible"
+                                label="Name"
+                                type="text"
+                                multiline
+                                //   value={value}
+                                //   onChange={handleChange}
+                                variant="outlined"
+                            />
+                        </Paper>
+
+                        </Grid>
+
+                        <Grid item xs={6}>
+
+                        <Paper className={classes.paper}>
+
+                            <TextField
+                                id="outlined-textarea"
+                                label="Email"
+                                type="email"
+                                variant="outlined"
+                            />
+                        </Paper>
+
+                        </Grid>
+
+                        <Grid item xs={6}>
+
+                        <Paper className={classes.paper}>
+
+                            <TextField
+                                id="outlined-multiline-static"
+                                label="Message"
+                                type="text"
+                                multiline
+                                rows={4}
+                                //   defaultValue="Default Value"
+                                variant="outlined"
+                            />
+                        </Paper>
+                        </Grid>
+                            <Button 
+                                size="large" 
+                                color="primary" 
+                                fullWidth="true" 
+                                variant="contained"
+                                endIcon={<SendIcon/>}
+                                >
+                                Submit
+                             </Button>
+                    </Grid>
+                </Grid>
             </div>
         </form>
     );
