e824370bd030ac32269bd45c674af4a68647eb0a
Ray H <rjhern37@neiu.edu>
2020-07-17 12:57:23 -0500

    Finished the project cards, need to finish home and contact page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,11 +8,12 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import { Link } from '@material-ui/core';
 
 
 const classes = makeStyles({
     root: {
-        maxWidth: 200,
+        maxWidth: 1000,
     },
     media: {
         height: 140,
@@ -25,14 +26,14 @@ function Projects() {
     return (
             <div className="project-container">
                 <div>
-                    <h3 className="project-title">This is where all the project titles go</h3>
+                    <div className="project-title"></div>
                     <Card className={classes.root}>
                             <CardMedia title="Portfolio #1" >
-                                <img src="https://cdn.motor1.com/images/mgl/P1j3K/s3/2020-chevrolet-corvette-first-drive.jpg" alt="First portfolio project"/>
+                            <img src="https://i.ibb.co/pKKK4s0/Plant-Shot.png" alt="Plant-Shot" border="0" width="100%"/>
                             </CardMedia>
                             <CardContent>
                                 <Typography gutterBottom variant="h5" component="h2">
-                                    #1
+                                    Plant Mobile
                         </Typography>
                                 <Typography variant="body2" color="textSecondary" component="p">
                                     Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
@@ -40,52 +41,29 @@ function Projects() {
                         </Typography>
                             </CardContent>
                         <CardActions>
+                        <Link href="https://rjhern37.github.io/Plant-Mobile/">
                             <Button size="small" color="primary">
-                                Share
-                    </Button>
+                                Find Food!
+                             </Button>
+                             </Link>
+                            <Link href="https://github.com/rjhern37/Plant-Mobile">
                             <Button size="small" color="primary">
-                                Learn More
-                    </Button>
+                                Github Repo
+                             </Button>
+                        </Link>
                         </CardActions>
                     </Card>
                 </div>
                 <div>
-                    <h3 className="project-title">This is where all the project titles go</h3>
-                    <Card className={classes.root}>
-                        <CardActionArea>
-                            <CardMedia title="Portfolio #1" >
-                                <img src="https://cdn.motor1.com/images/mgl/P1j3K/s3/2020-chevrolet-corvette-first-drive.jpg" alt="First portfolio project"/>
-                            </CardMedia>
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="h2">
-                                    #2
-                        </Typography>
-                                <Typography variant="body2" color="textSecondary" component="p">
-                                    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                                    across all continents except Antarctica
-                        </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                        <CardActions>
-                            <Button size="small" color="primary">
-                                Share
-                    </Button>
-                            <Button size="small" color="primary">
-                                Learn More
-                    </Button>
-                        </CardActions>
-                    </Card>
-                </div>
-                <div>
-                    <h3 className="project-title">This is where all the project titles go</h3>
+                    <div className="project-title"></div>
                     <Card className={classes.root}>
                         <CardActionArea>
                             <CardMedia title="Portfolio #1" >
-                                <img src="https://cdn.motor1.com/images/mgl/P1j3K/s3/2020-chevrolet-corvette-first-drive.jpg" alt="First portfolio project"/>
+                            <img src="https://i.ibb.co/vvx81M7/Coding-Resources-Shot.png" alt="Coding-Resources-Shot" width="100%"/> 
                             </CardMedia>
                             <CardContent>
                                 <Typography gutterBottom variant="h5" component="h2">
-                                    #3
+                                    Coding Resources
                         </Typography>
                                 <Typography variant="body2" color="textSecondary" component="p">
                                     Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
@@ -94,25 +72,29 @@ function Projects() {
                             </CardContent>
                         </CardActionArea>
                         <CardActions>
+                        <Link href="https://coding-resources-312.herokuapp.com/">
                             <Button size="small" color="primary">
-                                Share
-                    </Button>
+                                Get Started!
+                             </Button>
+                             </Link>
+                            <Link href="https://github.com/rjhern37/coding-resources">
                             <Button size="small" color="primary">
-                                Learn More
-                    </Button>
+                                Github Repo
+                             </Button>
+                        </Link>
                         </CardActions>
                     </Card>
                 </div>
                 <div>
-                    <h3 className="project-title">This is where all the project titles go</h3>
+                    <div className="project-title"></div>
                     <Card className={classes.root}>
                         <CardActionArea>
-                        <CardMedia title="Portfolio #1" >
-                                <img src="https://cdn.motor1.com/images/mgl/P1j3K/s3/2020-chevrolet-corvette-first-drive.jpg" alt="First portfolio project"/>
+                            <CardMedia title="Portfolio #1" >
+                            <img src="https://i.ibb.co/0sx5bLW/Directory-Shot.png" alt="Directory-Shot" border="0" width="100%"/>
                             </CardMedia>
                             <CardContent>
                                 <Typography gutterBottom variant="h5" component="h2">
-                                    #4
+                                    Awesome User Directory
                         </Typography>
                                 <Typography variant="body2" color="textSecondary" component="p">
                                     Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
@@ -121,25 +103,29 @@ function Projects() {
                             </CardContent>
                         </CardActionArea>
                         <CardActions>
+                        <Link href="https://sheltered-basin-68897.herokuapp.com/">
                             <Button size="small" color="primary">
-                                Share
-                    </Button>
+                                Get Started
+                             </Button>
+                             </Link>
+                            <Link href="https://github.com/rjhern37/Awesome-User-Directory2">
                             <Button size="small" color="primary">
-                                Learn More
-                    </Button>
+                                Github Repo
+                             </Button>
+                        </Link>
                         </CardActions>
                     </Card>
                 </div>
                 <div>
-                    <h3 className="project-title">This is where all the project titles go</h3>
+                    <div className="project-title"></div>
                     <Card className={classes.root}>
                         <CardActionArea>
                         <CardMedia title="Portfolio #1" >
-                                <img src="https://cdn.motor1.com/images/mgl/P1j3K/s3/2020-chevrolet-corvette-first-drive.jpg" alt="First portfolio project"/>
+                            <img src="https://i.ibb.co/m5qmm2B/Note-Taker-Shot.png" alt="Note-Taker-Shot" border="0" width="100%"/>
                             </CardMedia>
                             <CardContent>
                                 <Typography gutterBottom variant="h5" component="h2">
-                                    #5
+                                    Wonderful Note Taker Express
                         </Typography>
                                 <Typography variant="body2" color="textSecondary" component="p">
                                     Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
@@ -148,25 +134,29 @@ function Projects() {
                             </CardContent>
                         </CardActionArea>
                         <CardActions>
+                        <Link href="https://still-inlet-41032.herokuapp.com/">
                             <Button size="small" color="primary">
-                                Share
-                    </Button>
+                                What ya thinkin?
+                             </Button>
+                             </Link>
+                            <Link href="https://github.com/rjhern37/wonderful-note-taker-express">
                             <Button size="small" color="primary">
-                                Learn More
-                    </Button>
+                                Github Repo
+                             </Button>
+                        </Link>
                         </CardActions>
                     </Card>
                 </div>
                 <div>
-                    <h3 className="project-title">This is where all the project titles go</h3>
+                    <div className="project-title"></div>
                     <Card className={classes.root}>
                         <CardActionArea>
                         <CardMedia title="Portfolio #1" >
-                                <img src="https://cdn.motor1.com/images/mgl/P1j3K/s3/2020-chevrolet-corvette-first-drive.jpg" alt="First portfolio project"/>
+                        <img src="https://i.ibb.co/1q9fj1z/Fitness-Shot.png" alt="Fitness-Shot" border="0" width="100%"/>
                             </CardMedia>
                             <CardContent>
                                 <Typography gutterBottom variant="h5" component="h2">
-                                    #6
+                                    Fitness Tracker
                         </Typography>
                                 <Typography variant="body2" color="textSecondary" component="p">
                                     Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
@@ -175,12 +165,47 @@ function Projects() {
                             </CardContent>
                         </CardActionArea>
                         <CardActions>
+                            <Link href="https://fathomless-waters-80814.herokuapp.com/">
                             <Button size="small" color="primary">
-                                Share
-                    </Button>
+                                Get Started!
+                             </Button>
+                             </Link>
+                            <Link href="https://github.com/rjhern37/Fitness-Tracker">
                             <Button size="small" color="primary">
-                                Learn More
-                    </Button>
+                                Github Repo
+                             </Button>
+                             </Link>
+                        </CardActions>
+                    </Card>
+                </div>
+                <div>
+                    <div className="project-title"></div>
+                    <Card className={classes.root}>
+                        <CardActionArea>
+                        <CardMedia title="Portfolio #1" >
+                            <img src="https://i.ibb.co/7yMd0JG/Password-Shot.png" alt="Password-Shot" border="0" width="100%"/>
+                            </CardMedia>
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    Password Generator
+                        </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                                    across all continents except Antarctica
+                        </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                        <Link href="https://rjhern37.github.io/First-password-generator/">
+                            <Button size="small" color="primary">
+                                Generate Now!
+                             </Button>
+                             </Link>
+                            <Link href="https://github.com/rjhern37/First-password-generator">
+                            <Button size="small" color="primary">
+                                Github Repo
+                             </Button>
+                        </Link>
                         </CardActions>
                     </Card>
                 </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -55,66 +55,6 @@ function Home() {
                 </Card>
 
             </div>
-            <div className="home-container">
-
-                <Card className={classes.root}>
-                    <CardActionArea>
-                        <CardMedia
-                            className={classes.media}
-                            image="public/Simpson.jpg"
-                            title="Contemplative Reptile"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="h2">
-                                #1
-                        </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
-                                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                                across all continents except Antarctica
-                        </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                    <CardActions>
-                        <Button size="small" color="primary">
-                            Share
-                    </Button>
-                        <Button size="small" color="primary">
-                            Learn More
-                    </Button>
-                    </CardActions>
-                </Card>
-
-            </div>
-            <div className="home-container">
-
-                <Card className={classes.root}>
-                    <CardActionArea>
-                        <CardMedia
-                            className={classes.media}
-                            image="public/Simpson.jpg"
-                            title="Contemplative Reptile"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="h2">
-                                #1
-                        </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
-                                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                                across all continents except Antarctica
-                        </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                    <CardActions>
-                        <Button size="small" color="primary">
-                            Share
-                    </Button>
-                        <Button size="small" color="primary">
-                            Learn More
-                    </Button>
-                    </CardActions>
-                </Card>
-
-            </div>
 
         </div>
     )
