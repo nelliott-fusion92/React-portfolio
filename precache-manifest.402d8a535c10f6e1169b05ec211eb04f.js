31bd5d8bbd07ed5eec80b91ec2281640d0fab4d2
Ray H <rjhern37@neiu.edu>
2020-07-17 19:18:41 -0500

    Updates

---
+++ precache-manifest.402d8a535c10f6e1169b05ec211eb04f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf802b6106d5c5efe2e7e1582904a082",
+    "url": "/React-portfolio/index.html"
+  },
+  {
+    "revision": "83c8ac794136cc3794da",
+    "url": "/React-portfolio/static/css/main.1c748527.chunk.css"
+  },
+  {
+    "revision": "0c1007134b1cd7c67882",
+    "url": "/React-portfolio/static/js/2.01d3a721.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-portfolio/static/js/2.01d3a721.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83c8ac794136cc3794da",
+    "url": "/React-portfolio/static/js/main.977e83fc.chunk.js"
+  },
+  {
+    "revision": "fdc2cd998615ffd90855",
+    "url": "/React-portfolio/static/js/runtime-main.d1d5580a.js"
+  }
+]);
